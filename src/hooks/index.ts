b61d411ff5c550b02978f9a2f54348502580e87b
whoisaf <49568057+whoisaf@users.noreply.github.com>
2024-03-21 21:21:05 -0400

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,6 @@
 export * from "./useDoc";
 export { default as useDoc } from "./useDoc";
+export * from "./useCollection";
+export { default as useCollection } from "./useCollection";
+export * from "./useAuth";
+export { default as useAuth } from "./useAuth";
